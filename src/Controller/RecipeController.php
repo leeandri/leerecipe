b176e26d61c1f77de9c3e8fafeebc09c697b6539
leeandri <rl.tsirih@gmail.com>
2023-02-23 15:12:21 +0300

    ✨AImplementing the functionality - Writing down a recipe

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Mark;
 use App\Entity\Recipe;
+use App\Form\MarkType;
 use App\Form\RecipeType;
+use App\Repository\MarkRepository;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -65,11 +68,45 @@ class RecipeController extends AbstractController
      * @return Response
      */
     #[Security("is_granted('ROLE_USER')  and recipe.getIsPublic() === true")]
-    #[Route('/recipe/{id}', 'recipe.show', methods: ['GET'])]
-    public function show(Recipe $recipe): Response
-    {
+    #[Route('/recipe/{id}', 'recipe.show', methods: ['GET', 'POST'])]
+    public function show(
+        Recipe $recipe,
+        Request $request,
+        MarkRepository $repository,
+        EntityManagerInterface $manager
+    ): Response {
+        $mark = new Mark;
+        $form = $this->createForm(MarkType::class, $mark);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mark->setUser($this->getUser())
+                ->setRecipe($recipe);
+
+            $existingMark = $repository->findOneBy([
+                'user' => $this->getUser(),
+                'recipe' => $recipe
+            ]);
+
+            if (!$existingMark) {
+                $manager->persist($mark);
+            } else {
+                $existingMark->setMark($form->getData()->getMark());
+            }
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your mark was submitted successfully !'
+            );
+
+            return $this->redirectToRoute('recipe.show', ['id' => $recipe->getId()]);
+        }
+
         return $this->render('pages/recipe/show.html.twig', [
-            'recipe' => $recipe
+            'recipe' => $recipe,
+            'form' => $form->createView()
         ]);
     }
 
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Mark;
 use App\Entity\Recipe;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -70,14 +71,21 @@ class AppFixtures extends Fixture
                 $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
             }
 
-            $manager->persist($recipe);
-
             $recipes[] = $recipe;
 
             $manager->persist($recipe);
         }
 
-
+        // Marks
+        foreach ($recipes as $recipe) {
+            for ($m = 0; $m < mt_rand(0, 4); $m++) {
+                $mark = new Mark();
+                $mark->setMark(mt_rand(1, 5))
+                    ->setUser($users[mt_rand(0, count($users) - 1)])
+                    ->setRecipe($recipe);
+                $manager->persist($mark);
+            }
+        }
 
         $manager->flush();
     }
